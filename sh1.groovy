52c8cef6ea1200da82c3cdfb243499926ae24306
Praful <dhoblepraful31@gmail.com>
2022-07-14 14:59:09 +0530

    url echo

--- sh1.groovy
+++ sh1.groovy
@@ -4,10 +4,10 @@ job('demo2byGit') {
             remote {
                 name('origin')
                 url('https://github.com/praful31/Jenkins.git')
-               }
+            }
         }
     }
     steps {
-         sh 'echo \'successfully deployed\' >> home.txt' 
+         shell('echo successfully implemented item')
     }
-} 
+}
